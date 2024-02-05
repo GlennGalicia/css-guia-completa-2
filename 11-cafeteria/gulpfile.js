e0d1a6da66b0533a826f827052c342ae3b481553
Glenn Galicia <glenn.f.g.a@gmail.com>
2024-02-05 11:29:49 -0600

    11-cafeteria - se agregaron dependencias y funciones para manejar imagenes tipo avif y webp

--- 11-cafeteria/gulpfile.js
+++ 11-cafeteria/gulpfile.js
@@ -1,7 +1,14 @@
-const { src, dest, watch, series, parallel } = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
+const { src, dest, watch, series, parallel } = require('gulp');
+
+// CSS y SASS
+const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+// Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
     // Compilar Sass
@@ -15,11 +22,35 @@ function css(done) {
     done()
 }
 
+function imagenes() {
+    return src('src/img/**/*')
+        .pipe(imagemin({ optimizationLevel: 3 }))
+        .pipe(dest('build/img'))
+}
+
+function imagenWebp() {
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(webp())
+        .pipe(dest('build/img'))
+}
+
+function imagenAvif() {
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(avif())
+        .pipe(dest('build/img'))
+}
+
 function dev() {
     watch('src/scss/**/*.scss', css)
+    watch('src/img/**/*', imagenes)
 }
+
+exports.css = css;
+exports.dev = dev;
+exports.imagenes = imagenes;
+exports.imagenWebp = imagenWebp;
+exports.imagenAvif = imagenAvif;
 
 // series - Se inicia una nueva tarea, y hasta que finaliza comienza la siguiente
 // parallel - Todas inicial al mismo tiempo
-
-exports.default = series (css, dev)
+exports.default = series(imagenes, imagenWebp, imagenAvif, css, dev);
